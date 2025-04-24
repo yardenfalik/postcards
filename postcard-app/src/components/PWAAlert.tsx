8d313480f6f3e7c3f22b58198d0c977be75ef9d6
Your name here <yardenfalik@gmail.com>
2025-04-24 13:23:21 +0300

    fixing font and icons

--- postcard-app/src/components/PWAAlert.tsx
+++ postcard-app/src/components/PWAAlert.tsx
@@ -1,6 +1,7 @@
 import "./components-css/PWAAlert.css"
 import { useState } from "react";
 import shareIcon from "../assets/shareBtn.png";
+import xIcon from "../assets/XIcon.png";
 
 export function PWAAlert() {
     const [isAlertVisible, setIsAlertVisible] = useState(true);
@@ -13,8 +14,8 @@ export function PWAAlert() {
     <>
         { isAlertVisible ?
             <div className="pwa-alert" onClick={handleInstallClick}>
-                <p>Install the app, tap on <img src={shareIcon} alt="share_button" /> then select Add to Home Screen</p>
-                <button onClick={handleInstallClick}>X</button>
+                <p>Install the app, tap on <img className="shareIcon" src={shareIcon} alt="share_button" /> then select Add to Home Screen</p>
+                <button onClick={handleInstallClick}><img className="xIcon" src={xIcon} alt="x" /></button>
             </div> 
         : null }
     </>
